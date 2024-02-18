d43eb31383ffd2b6634e6705606048d8bb2897a0
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-18 03:12:14 +0000

    initial devise install set up complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "boards", :action => "index" })
+  devise_for :users
+
+  root "boards#index"
+
+  #get("/", { :controller => "boards", :action => "index" })
 
   # Routes for the Post resource:
 
@@ -42,5 +46,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "boards#index"
 end
